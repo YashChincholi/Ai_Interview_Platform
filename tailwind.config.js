31924233aedc5a1d9e3bd03765fc0333e64134ed
Yash Chincholi <yashchincholi18@gmail.com>
2025-03-27 22:17:40 +0530

    Auth Forms Setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcssAnimate],
+};
